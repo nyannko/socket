934e9791b1ee92f0dd3092bb07541f1e833b4105
nyannko <9638293+nyannko@users.noreply.github.com>
2018-03-14 23:13:49 +0100

    add: dns_resolver

--- Python/DNS/dns_resolver.py
+++ Python/DNS/dns_resolver.py
@@ -22,6 +22,7 @@ import struct
 QCLASS_IN = 1
 QTYPE_A = 1
 
+
 def get_resolv():
     with open('/etc/resolv.conf') as f:
         f = f.readlines()
@@ -32,6 +33,7 @@ def get_resolv():
             line = line.split()[1]
             dns_servers.append(line)
         return dns_servers
+
 
 def build_qname(address):
     address = address.strip(b'.')
@@ -46,17 +48,59 @@ def build_qname(address):
     # [b'\x03', b'www', b'\x06', b'google', b'\x03', b'com', b'\x00']
     return b''.join(results)
 
+
 def build_query(domain_name, qtype):
     id = os.urandom(2)
-    remaining_header = struct.pack('!BBHHHH', 1,0,1,0,0,0)
+    remaining_header = struct.pack('!BBHHHH', 1, 0, 1, 0, 0, 0)
     qname = build_qname(domain_name)
     qtype_qclass = struct.pack('!HH', qtype, QCLASS_IN)
     return id + remaining_header + qname + qtype_qclass
 
 
-def parse_record(res, offset):
-    # record_type, record_class, record_ttl, record_rdlength = struct.unpack("!HHiH", res[])
-    pass
+def parse_name(data, offset):
+    p = offset
+    labels = []
+    # \x03 length of label
+    l = data[p]
+    while l > 0:
+        # \xc0\x0c pointer
+        if (l & (128 + 64)) == (128 + 64):
+            pointer = struct.unpack('!H', data[p:p + 2])[0]
+            # the first two bits of offset
+            pointer &= 0x3FFF
+            r = parse_name(data, pointer)
+            labels.append(r[1])
+            p += 2
+            # pointer is the end
+            return p - offset, b'.'.join(labels)
+        else:
+            labels.append(data[p + 1:p + 1 + l])
+            p += 1 + l
+        l = data[p]
+    return p - offset + 1, b'.'.join(labels)
+
+
+def parse_record(data, offset, question=False):
+    nlen, name = parse_name(data, offset)
+    if not question:
+        record_type, record_class, record_ttl, record_rdlength = struct.unpack(
+            '!HHiH', data[offset + nlen:offset + nlen + 10]
+        )
+        ip = parse_ip(record_type, data, record_rdlength, offset + nlen + 10)
+        return nlen + 10 + record_rdlength, \
+               (name, ip, record_type, record_class, record_ttl)
+    else:
+        record_type, record_class = struct.unpack(
+            '!HH', data[offset + nlen:offset + nlen + 4]
+        )
+        return nlen + 4, (name, None, record_type, record_class, None, None)
+
+
+def parse_ip(addrtype, data, length, offset):
+    if addrtype == QTYPE_A:
+        return socket.inet_ntop(socket.AF_INET, data[offset:offset + length])
+
+
 def parse_header(res):
     header = struct.unpack("!HHBBHHH", res[:12])
     res_id = header[0]
@@ -70,18 +114,43 @@ def parse_header(res):
     res_arcount = header[6]
     return (res_id, res_qr, res_tc, res_ra, res_rcode, res_qdcount, res_ancount, res_nscount, res_arcount)
 
-def parse_response(res):
-    if(len(res) >= 12):
-        header = parse_header(res)
-        res_id, res_qr, res_tc, res_ra, res_rcode, res_qdcount, res_ancount, res_nscount, res_arcount = header
-    offset = 12
-    print(header)
-    
-    #for i in range(0, res_qdcount):
+
+def parse_response(data):
+    try:
+        if len(data) >= 12:
+            header = parse_header(data)
+            if not header:
+                return None
+            res_id, res_qr, res_tc, res_ra, res_rcode, res_qdcount, \
+            res_ancount, res_nscount, res_arcount = header
+
+            qds = []
+            ans = []
+            offset = 12
+            for i in range(0, res_qdcount):
+                l, r = parse_record(data, offset, True)
+                offset += l
+                if r:
+                    qds.append(r)
+            for i in range(0, res_ancount):
+                l, r = parse_record(data, offset)
+                offset += l
+                if r:
+                    ans.append(r)
+            for i in range(0, res_nscount):
+                l, r = parse_record(data, offset)
+                offset += l
+            for i in range(0, res_arcount):
+                l, r = parse_record(data, offset)
+                offset += l
+            print(qds)
+            print(ans)
+    except Exception as e:
+        return None
 
 
 def resolve(host):
-    if(host != bytes):
+    if (host != bytes):
         host = host.encode('utf-8')
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -90,12 +159,13 @@ def resolve(host):
 
     dns_servers = get_resolv()
     sock.sendto(query, ('8.8.8.8', 53))
-    #sock.sendto(query, (dns_servers[0], 53))
+    # sock.sendto(query, (dns_servers[0], 53))
 
     res, addr = sock.recvfrom(1024)
     print(res)
 
     parse_response(res)
+
 
 resolve('www.google.com')
 # Answer:
